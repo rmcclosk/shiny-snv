5172302cd25e9e1fd6bbd3b057fc24d806acfff5
Rosemary McCloskey <rmcclosk.math@gmail.com>
2014-11-04 16:56:38 -0800

    Collect indel VAF (see #6).

--- README.md
+++ README.md
@@ -1,4 +1,21 @@
-shiny-snv
-=========
+Dependencies
+============
 
-Shiny visualization of colorectal cancer SNVs.
+This visualization requires several pieces of software.
+
+- R version 3.1.1 or higher
+- Python version 2.7.8 or higher
+- the Pysam python library
+
+In addition, several R libraries are used.
+
+- shiny
+- ggvis
+
+Running the Visualization
+=========================
+
+Edit "metadata.tsv" for your data. The "time.point" column is used to
+differentiate samples taken contemporaneously vs. from different time points.
+These should be numbered in chronological order, but the actual numbers used
+are not important. The special time point 0 indicates a normal sample.
--- collect_vaf.py
+++ collect_vaf.py
@@ -8,9 +8,78 @@ import os
 import re
 import argparse
 import warnings
+import subprocess
+import tempfile
+import itertools
+from Bio import SeqIO
 
 NUCLEOTIDES = ["A", "T", "C", "G"]
 NA_STRINGS = ["NA", "None", ""]
+
+def num_mismatches(ref, query):
+    """Number of matches between query and reference."""
+    mismatches = 0
+    for i in range(len(ref)):
+        if query[i] != ref[i]:
+            mismatches += 1
+    return mismatches
+
+def multi_align(ref1, ref2, queries):
+    """
+    Do MSA of several query sequences to two references. Return the reference
+    index with the fewest mismatches for each sequence.
+    """
+    tfile = tempfile.NamedTemporaryFile()
+    tfile.write(">ref1\n{}\n".format(ref1))
+    tfile.write(">ref2\n{}\n".format(ref2))
+    n_queries = 0
+    for query in queries:
+        tfile.write(">query{}\n{}\n".format(n_queries, query))
+        n_queries += 1
+    tfile.flush()
+    
+    outfile = tempfile.TemporaryFile()
+    cmd = ["mafft", "--auto", tfile.name]
+    subprocess.Popen(cmd, stdout=outfile, stderr=subprocess.PIPE).communicate()
+    outfile.seek(0)
+    align = SeqIO.parse(outfile, "fasta")
+
+    ref1 = next(align)
+    ref2 = next(align)
+    scores = [0, 0]
+    ties = 0
+    for query in align:
+        mismatch1 = num_mismatches(ref1, query)
+        mismatch2 = num_mismatches(ref2, query)
+        if mismatch1 < mismatch2:
+            scores[0] += 1
+        elif mismatch1 > mismatch2:
+            scores[1] += 1
+        else:
+            ties += 1
+    if ties > 0:
+        msg = "Discarding {} reads aligning equally to ref and alt".format(ties)
+        warnings.warn(msg)
+
+    return scores
+
+def count_indels(samfile, reffile, chrom, pos, ref, alt, min_mapq=20):
+    """
+    Count occurences of the reference and alternate indel allele at a given
+    position, by alignment score.
+    """
+    start = max(pos-100, 0)
+    end = pos+100
+    ref_seq = reffile.fetch(reference=chrom, start=start, end=end).decode("utf-8")
+    alt_seq = ref_seq[:pos-start-1] + alt + ref_seq[pos-start-1+len(ref):]
+    
+    reads = samfile.fetch(chrom, pos, pos+len(ref))
+    counts = [0, 0]
+    
+    reads = [r.seq for r in reads if r.mapq >= min_mapq]
+    scores = multi_align(ref_seq, alt_seq, reads)
+    
+    return scores
 
 def count_bases_pileup(pileup, position, min_baseq=15, min_mapq=20):
     """
@@ -61,6 +130,13 @@ def read_maf_file(path):
             rows[key] = row
     return rows
 
+def dict_append(d, key, value):
+    try:
+        d[key].append(value)
+    except KeyError:
+        d[key] = [value]
+    return d
+
 if __name__ == "__main__":
 
     desc = "Collect VAF from multiple BAM files."
@@ -84,16 +160,19 @@ if __name__ == "__main__":
     for row in reader:
         patient = row["patient"]
         sample = row["sample"]
-        maf_file = row["maf.file"]
+        snv_maf = row["snv.maf"]
+        indel_maf = row["indel.maf"]
         bam_file = row["bam.file"]
 
-        if os.path.exists(maf_file):
-            try:
-                maf_files[patient].append(maf_file)
-            except KeyError:
-                maf_files[patient] = [maf_file]
-        elif maf_file not in NA_STRINGS:
-            warnings.warn("MAF file {} does not exist".format(maf_file))
+        if os.path.exists(snv_maf):
+            maf_files = dict_append(maf_files, patient, snv_maf)
+        elif snv_maf not in NA_STRINGS:
+            warnings.warn("MAF file {} does not exist".format(snv_maf))
+
+        if os.path.exists(indel_maf):
+            maf_files = dict_append(maf_files, patient, indel_maf)
+        elif indel_maf not in NA_STRINGS:
+            warnings.warn("MAF file {} does not exist".format(indel_maf))
 
         if os.path.exists(bam_file):
             try:
@@ -125,10 +204,16 @@ if __name__ == "__main__":
                 row["chrom"], row["pos"], row["ref"], row["alt"] = key
 
                 # get allele counts
-                counts = count_bases(samfile, reffile, row["chrom"], int(row["pos"]))
-                row["ref.count"] = counts[in_row["Reference_Allele"]]
-                row["alt.count"] = counts[in_row["Tumor_Seq_Allele1"]]
-                row["depth"] = sum(counts.values())
+                if len(row["ref"]) == len(row["alt"]) == 1: # snv
+                    counts = count_bases(samfile, reffile, row["chrom"], int(row["pos"]))
+                    row["ref.count"] = counts[in_row["Reference_Allele"]]
+                    row["alt.count"] = counts[in_row["Tumor_Seq_Allele1"]]
+                    row["depth"] = sum(counts.values())
+                else: # indel
+                    counts = count_indels(samfile, reffile, row["chrom"], int(row["pos"]), row["ref"], row["alt"])
+                    row["ref.count"] = counts[0]
+                    row["alt.count"] = counts[1]
+                    row["depth"] = sum(counts)
 
                 # info from MAF file
                 row["gene"] = in_row["Hugo_Symbol"]
@@ -136,6 +221,9 @@ if __name__ == "__main__":
                 row["cosmic"] = ",".join(re.findall("COSM\d+", in_row["dbSNP_RS"]))
                 row["rs"] = ",".join(re.findall("rs\d+", in_row["dbSNP_RS"]))
                 row["esp"] = ",".join(re.findall("TMP_ESP_[\dXY]{1,2}_\d+", in_row["dbSNP_RS"]))
-                row["prot.change"] = in_row["Protein_Change"].replace("p.", "")
+                if in_row["Protein_Change"] is None:
+                    row["prot.change"] = ""
+                else:
+                    row["prot.change"] = in_row["Protein_Change"].replace("p.", "")
 
                 writer.writerow(row)
